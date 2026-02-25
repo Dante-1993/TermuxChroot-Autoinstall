a74b8f3e11f228263996bb4365f86da88bb4cbee
Dante-1993 <73445345+Dante-1993@users.noreply.github.com>
2026-02-25 15:15:59 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+pkg update
+pkg install x11-repo
+pkg install root-repo
+pkg install termux-x11-nightly
+pkg update
+pkg install tsu
+pkg install pulseaudio
+chmod +x ./install.sh
+su -c ./install.sh
